3c57aaa20a4f85fce9e18d604e19ad2f69793ac4
Petr Minkin <isensay@gmail.com>
2026-03-03 19:29:03 +0300

    commit

--- public/index.php
+++ public/index.php
@@ -3,27 +3,18 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
-define("LARAVEL_START", microtime(true));
+define('LARAVEL_START', microtime(true));
 
-if (file_exists($maintenance = __DIR__."/../storage/framework/maintenance.php")) {
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// СНАЧАЛА загружаем autoloader
-require __DIR__."/../vendor/autoload.php";
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
 
-// ПОТОМ загружаем .env (теперь Dotenv доступен)
-try {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
-    $dotenv->load();
-    // Для отладки
-    $_ENV = array_merge($_ENV, $_SERVER);
-} catch (Exception $e) {
-    // Логируем ошибку но продолжаем
-    error_log("Dotenv load warning: " . $e->getMessage());
-}
-
+// Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__."/../bootstrap/app.php";
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
